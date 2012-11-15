c2e24356b060172b756ba8f2ede6f56d032cb43d
root <root@hunter.puppetlabs.lan>
2012-11-15 06:26:02 +0000

    Adding first version of visudo

--- modules/sudoers/manifests/init.pp
+++ modules/sudoers/manifests/init.pp
@@ -1,9 +1,24 @@
-class sudoers {
-  file { '/etc/sudoers':
+class sudoers($tmpfile = '/tmp/sudoers') {
+  File {
     ensure => present,
     owner  => 'root',
     group  => 'root',
     mode   => '0440',
+  }
+  file { $tmpfile:
     source => 'puppet:///modules/sudoers/sudoers',
   }
+
+  exec { 'visudo sudoers':
+    command => "visudo -c -f ${tmpfile}",
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+    unless  => "diff /etc/sudoers ${tmpfile}",
+  }
+
+  file { '/etc/sudoers':
+    source  => $tmpfile,
+    require => Exec['visudo sudoers'],
+  }
+
+  # Something with visudo -c -f /tmp/sudoers
 }
